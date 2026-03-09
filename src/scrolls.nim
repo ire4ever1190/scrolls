afde83ccf6e77ddee787ff14ca024109ec5cd01d
Jake Leahy <jake@leahy.dev>
2026-03-09 13:01:06 +1100

    Add some docs

--- src/scrolls.nim
+++ src/scrolls.nim
@@ -1,3 +1,8 @@
 import ./scrolls/[envProvider, configReader]
 
 export envProvider, configReader
+
+## .. importdoc:: scrolls/envProvider
+## This library abstracts reading configuration files so that there is a single interface
+## for reading but multiple backwards to pull values from. Current supported backends are
+## - [EnvProvider] which reads from environment variables
